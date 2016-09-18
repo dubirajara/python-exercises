d3e2257995e382f3886859aea60aebb8e12e5361
Diego <diego@MacBook-Pro-de-Diego.local>
2016-09-18 12:33:05 +0200

    refactoring

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,53 +1,49 @@
 import random
 
-
 jokenpo = ['rock', 'paper', 'scissors']
 random.shuffle(jokenpo)
 
+
 def select():
-	print('\nSelect: "Rock, Paper or Scissors"')
-	player = input('> ').lower()
-	enemy = random.choice(jokenpo)
+    print('\nSelect: "Rock, Paper or Scissors"')
+    player = input('> ').lower()
+    enemy = random.choice(jokenpo)
 
-	if player in jokenpo:
+    if player in jokenpo:
 
-		if player == enemy:
-			print('\nDraw!!!')
+        if player == enemy:
+            print('\nDraw!!!')
 
-		elif player == 'rock' and enemy == 'scissors':
-			print('\nYou Win, {0} breaks {1}'.format(player, enemy))
+        elif player == 'rock' and enemy == 'scissors':
+            print('\nYou Win, {0} breaks {1}'.format(player, enemy))
 
-		elif player == 'paper' and enemy == 'rock':
-			print('\nYou Win, {0} covers {1}'.format(player, enemy))
+        elif player == 'paper' and enemy == 'rock':
+            print('\nYou Win, {0} covers {1}'.format(player, enemy))
 
-		elif player == 'scissors' and enemy == 'paper':
-			print('\nYou Win, {0} cuts {1}'.format(player, enemy))
+        elif player == 'scissors' and enemy == 'paper':
+            print('\nYou Win, {0} cuts {1}'.format(player, enemy))
 
-		else:
-			print('\nYou lose!!! you: {0} and the machine win: {1}'.format(player, enemy))
-		
-		try_again()
+        else:
+            print('\nYou lose!!! you: {0} and the machine win: {1}'.format(player, enemy))
 
-	else:
-		select()
+        try_again()
+
+    else:
+        select()
 
 
 def try_again():
-	a = input("\nDo you want to keep playing y/n ? ")
+    a = input("\nDo you want to keep playing y/n ? ")
 
-	while a.lower() != 'y' and a.lower() != 'n':
-		a = input("\nDo you want to keep playing y/n ? ")
+    while a.lower() != 'y' and a.lower() != 'n':
+        a = input("\nDo you want to keep playing y/n ? ")
 
-	if a.lower() == ("y"):
-		select()
+    if a.lower() == ("y"):
+        select()
 
-	else:
-		print("\nGame Over!!!")
-
-
-        
-select()
+    else:
+        print("\nGame Over!!!")
 
 
-
-
+if __name__ == "__main__":
+    select()
